d0f7ac1f630ed1d811524429941ca0d4271e8e8d
mgst-d <and8511@ya.ru>
2021-08-10 20:32:49 +0300

    Add functions: add_to_cart and get_number_of_item

--- public/app.js
+++ public/app.js
@@ -7,5 +7,22 @@ function something() {
 }
 
 function add_to_cart(id) {
-	alert('You added pizza with id=' + id);
+	var x = window.localStorage.getItem('product_' + id);
+	x = x * 1 + 1;
+	window.localStorage.setItem('product_' + id, x);
+	// alert('You added pizza with id=' + id);
+}
+function cart_get_number_of_items()
+{
+	var count = 0;
+	for(var i=0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i);
+		if (key.indexOf('product_') == 0)
+		{
+			var value = window.localStorage.getItem(key);
+			count = count + value * 1;
+		}
+	}
+	return count;
 }
