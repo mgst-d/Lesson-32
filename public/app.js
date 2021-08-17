49b3521624ed32881cb23479e18c04723a1f053d
mgst-d <and8511@ya.ru>
2021-08-17 18:49:11 +0300

    Fix bug

--- public/app.js
+++ public/app.js
@@ -49,6 +49,6 @@ function update_orders()
 }
 function update_orders_button()
 {
-	var text = 'Cart (' + cart_get_number_of_items + ')';
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
 	$('#orders_button').val(text);
 }
